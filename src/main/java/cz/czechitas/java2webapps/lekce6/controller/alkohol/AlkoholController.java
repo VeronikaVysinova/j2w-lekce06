7836aa14d07419acb8f532757842c191f277073f
Veronika <v@email.com>
2025-10-23 20:41:49 +0200

    -all TODO done

--- src/main/java/cz/czechitas/java2webapps/lekce6/controller/alkohol/AlkoholController.java
+++ src/main/java/cz/czechitas/java2webapps/lekce6/controller/alkohol/AlkoholController.java
@@ -27,18 +27,18 @@ public class AlkoholController {
     }
 
     @PostMapping("")
-    public Object form(@Valid @ModelAttribute("form") AlkoholForm form, BindingResult bindingResult) {
+    public Object form(@Valid @ModelAttribute("form") AlkoholForm form, BindingResult bindingResult) { //bindingResult - tam se ulozi zadana data
 /*
     if (form.getVek() < 18) {
       return "/alkohol/nizky-vek";
     }
 */
 
-        if (bindingResult.hasErrors()) {
-            return "/alkohol/formular";
+        if (bindingResult.hasErrors()) { //metoda, ktera rika, jestli tam jsou chyby. Kdyz ano, zobrazi se znovu ten formular a zobrazi ty chyby v okne s formularem
+            return "/alkohol/formular"; //nemam databazi, tak pouzivam zobrazeni stranky s vysledkem, ale mela bych pouzivat redirect
         }
 
-        return new ModelAndView("/alkohol/objednano")
+        return new ModelAndView("/alkohol/objednano")  //vracim hodnoty rovnou z formulare ale do budoucna budeme hodnoty vracet z databaze
                 .addObject("kod", Math.abs(random.nextInt()))
                 .addObject("email", form.getEmail());
     }
